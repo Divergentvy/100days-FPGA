d4d21237ea7c9439334a2e66d2d7d2e76ae1aaa7
Divergent767 <138713133+Divergent767@users.noreply.github.com>
2026-07-28 02:14:21 +0530

    encoder

---
+++ encoder.v
@@ -0,0 +1,21 @@
+module encoder(I,Y);
+
+input [3:0]I;
+output reg [1:0]Y;
+
+always @(*)begin
+if(I[3]==1)begin
+Y = 2'b11;
+end else if(I[2]==1) begin
+Y = 2'b10;
+end else if(I[1]==1)begin
+Y = 2'b01;
+end else if(I[0]==1)begin
+Y = 2'b00;
+end else begin
+Y = 2'bxx;
+end
+
+end
+
+endmodule
